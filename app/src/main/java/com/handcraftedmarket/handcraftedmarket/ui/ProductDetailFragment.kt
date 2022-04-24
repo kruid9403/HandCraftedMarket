cfb89d049f44388245017f0bb2ff8041f9e9498d
Jeremy <jeremy.kruid@jedmahonisgroup.com>
2022-04-24 11:36:56 -0500

    intellij

--- app/src/main/java/com/handcraftedmarket/handcraftedmarket/ui/ProductDetailFragment.kt
+++ app/src/main/java/com/handcraftedmarket/handcraftedmarket/ui/ProductDetailFragment.kt
@@ -52,45 +52,36 @@ class ProductDetailFragment : Fragment(),
         // Inflate the layout for this fragment
         binding = FragmentProductDetailBinding.inflate(layoutInflater)
 
-        if (arguments!= null){
-            product = arguments?.get("product") as Product
-        }
-
-        viewModel = ViewModelProvider(this).get(ProductDetailVM::class.java)
-
-        setViews()
-
-        binding.shopViewImageRecycler.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
-        photoAdapter = ShopViewPhotoAdapter(requireContext(), this, product.imgUrl)
-        binding.shopViewImageRecycler.adapter = photoAdapter
-
-        standardAdapter = ShopViewStandardAdapter(requireContext(), this, product.productStandard)
-        binding.shopViewStandard.layoutManager = LinearLayoutManager(requireContext())
-        binding.shopViewStandard.adapter = standardAdapter
-
-        binding.shopViewCustomize.layoutManager = LinearLayoutManager(requireContext())
-        customAdapter = ShopViewCustomAdapter(requireContext(), this, product.options)
-        binding.shopViewCustomize.adapter = customAdapter
-
-        binding.productDetailAddToCart.setOnClickListener(this)
-
-        viewModel.cartList.observe(viewLifecycleOwner){
-            if (it != null && it.cartList != cart.cartList) {
-                cart = it
-            }
-
-        }
+//        if (arguments!= null){
+//            product = arguments?.get("product") as Product
+//        }
+//
+//        viewModel = ViewModelProvider(this).get(ProductDetailVM::class.java)
+//
+//        setViews()
+//
+//        binding.shopViewImageRecycler.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+//        photoAdapter = ShopViewPhotoAdapter(requireContext(), this, product.imgUrl)
+//        binding.shopViewImageRecycler.adapter = photoAdapter
+//
+//        standardAdapter = ShopViewStandardAdapter(requireContext(), this, product.productStandard)
+//        binding.shopViewStandard.layoutManager = LinearLayoutManager(requireContext())
+//        binding.shopViewStandard.adapter = standardAdapter
+//
+//        binding.shopViewCustomize.layoutManager = LinearLayoutManager(requireContext())
+////        customAdapter = ShopViewCustomAdapter(requireContext(), this, product.options)
+//        binding.shopViewCustomize.adapter = customAdapter
+//
+//        binding.productDetailAddToCart.setOnClickListener(this)
+//
+//        viewModel.cartList.observe(viewLifecycleOwner){
+//            if (it != null && it.cartList != cart.cartList) {
+//                cart = it
+//            }
+//
+//        }
 
         return binding.root
-    }
-
-    fun setViews(){
-        Glide.with(requireContext()).load(product.imgUrl[0]).into(binding.shopViewMainImg)
-        binding.shopViewName.text = product.name
-        binding.shopViewDescription.text = product.description
-        val format = NumberFormat.getCurrencyInstance()
-        format.maximumFractionDigits = 2
-        binding.shopViewPrice.text = format.format(product.price)
     }
 
     override fun onOptionClicked(photo: String, position: Int) {
@@ -103,63 +94,85 @@ class ProductDetailFragment : Fragment(),
     override fun onStandardClicked(photo: String, position: Int) {
     }
 
-    override fun onClick(v: View?) {
-        when(v){
-            binding.productDetailAddToCart -> {
-                product.purchaseQty += 1
-                if (firebaseManager.auth.currentUser != null) {
-                    cart = viewModel.cartList.value!!
-                    if (cart == CartList()){
-                        cart = CartList(arrayListOf(Cart(product.creator, arrayListOf(product))))
-                        viewModel.cartList.postValue(cart)
-                    }else if (cart.cartList.size == 0){
-                        Log.e("ProductDetailFrag", "click")
-                        cart.cartList.add(Cart(product.creator, arrayListOf(product)))
-                        viewModel.cartList.postValue(cart)
-                        Toast.makeText(requireContext(), "ug", Toast.LENGTH_SHORT).show()
-                    }else{
-                        cart.cartList.forEach {
-                            if (it.creator == product.creator){
-                                var exists = false
-                                it.products.forEach { prod ->
-                                    if (prod.id == product.id){
-                                        Toast.makeText(requireContext(), "Product is already in your cart", Toast.LENGTH_SHORT)
-                                            .show()
-                                        exists = true
-                                    }
-                                }
-
-                                if (!exists) {
-                                    it.products.add(product)
-                                    viewModel.cartList.postValue(cart)
-                                    Toast.makeText(requireContext(), "fuck", Toast.LENGTH_SHORT)
-                                        .show()
-                                }
-                            }
-                        }
-                    }
-
-                    firebaseManager.cart().set(cart).continueWith { result ->
-                        if (result.isSuccessful) {
-                            Toast.makeText(requireContext(), "Added to Cart", Toast.LENGTH_SHORT)
-                                .show()
-                        } else {
-                            Toast.makeText(
-                                requireContext(),
-                                "An error has occurred, Try Again",
-                                Toast.LENGTH_SHORT
-                            ).show()
-                            Log.e("ProductDetail", result.exception.toString())
-                        }
-                    }
-
-                }else{
-                    val bundle = bundleOf(
-                        "product" to product
-                    )
-                    findNavController().navigate(R.id.loginFragment, bundle)
-                }
-            }
-        }
+    override fun onClick(p0: View?) {
     }
+
+//    fun setViews(){
+//        Glide.with(requireContext()).load(product.imgUrl[0]).into(binding.shopViewMainImg)
+//        binding.shopViewName.text = product.name
+//        binding.shopViewDescription.text = product.description
+//        val format = NumberFormat.getCurrencyInstance()
+//        format.maximumFractionDigits = 2
+//        binding.shopViewPrice.text = format.format(product.price)
+//    }
+//
+//    override fun onOptionClicked(photo: String, position: Int) {
+//
+//    }
+//
+//    override fun onPhotoClicked(photo: String, position: Int) {
+//    }
+//
+//    override fun onStandardClicked(photo: String, position: Int) {
+//    }
+//
+//    override fun onClick(v: View?) {
+//        when(v){
+//            binding.productDetailAddToCart -> {
+//                product.purchaseQty += 1
+//                if (firebaseManager.auth.currentUser != null) {
+//                    cart = viewModel.cartList.value!!
+//                    if (cart == CartList()){
+//                        cart = CartList(arrayListOf(Cart(product.creator, arrayListOf(product))))
+//                        viewModel.cartList.postValue(cart)
+//                    }else if (cart.cartList.size == 0){
+//                        Log.e("ProductDetailFrag", "click")
+//                        cart.cartList.add(Cart(product.creator, arrayListOf(product)))
+//                        viewModel.cartList.postValue(cart)
+//                        Toast.makeText(requireContext(), "ug", Toast.LENGTH_SHORT).show()
+//                    }else{
+//                        cart.cartList.forEach {
+//                            if (it.creator == product.creator){
+//                                var exists = false
+//                                it.products.forEach { prod ->
+//                                    if (prod.id == product.id){
+//                                        Toast.makeText(requireContext(), "Product is already in your cart", Toast.LENGTH_SHORT)
+//                                            .show()
+//                                        exists = true
+//                                    }
+//                                }
+//
+//                                if (!exists) {
+//                                    it.products.add(product)
+//                                    viewModel.cartList.postValue(cart)
+//                                    Toast.makeText(requireContext(), "fuck", Toast.LENGTH_SHORT)
+//                                        .show()
+//                                }
+//                            }
+//                        }
+//                    }
+//
+//                    firebaseManager.cart().set(cart).continueWith { result ->
+//                        if (result.isSuccessful) {
+//                            Toast.makeText(requireContext(), "Added to Cart", Toast.LENGTH_SHORT)
+//                                .show()
+//                        } else {
+//                            Toast.makeText(
+//                                requireContext(),
+//                                "An error has occurred, Try Again",
+//                                Toast.LENGTH_SHORT
+//                            ).show()
+//                            Log.e("ProductDetail", result.exception.toString())
+//                        }
+//                    }
+//
+//                }else{
+//                    val bundle = bundleOf(
+//                        "product" to product
+//                    )
+//                    findNavController().navigate(R.id.loginFragment, bundle)
+//                }
+//            }
+//        }
+//    }
 }
